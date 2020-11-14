41a162436d146c8e08026ee1b90444d1fae438c7
taxuda <trandat.student>
2020-11-14 17:38:13 +0100

    try catch in Schleife; Chay tiep ct khi gap loi; parseInt return Int 10 base 4 1*4 + 0*4^0 = 4

--- uebung/uebung4/Exceptions.java
+++ uebung/uebung4/Exceptions.java
@@ -6,19 +6,22 @@ public class Exceptions {
         numbers[0] = "10";
         numbers[1] = "20";
         numbers[2] = "30";
-        try{
-            for(int base = 10; base >= 2; base--){
-                for(int i = 0; i <= 3; i++){
-                    int j = Integer.parseInt(numbers[i],base);
-                    System.out.println(numbers[i]+"base "+base+" = "+j);
+
+        for(int base = 10; base >= 2; --base){
+            for(int i = 0; i <= 3; i++){
+                try {
+                    // fehler 1: typ int // int j = Integer.parseInt(numbers[i], base);
+                    int j = Integer.parseInt(numbers[i], base);
+                    System.out.println(numbers[i] + "base " + base + " = " + j);
+                }
+                catch (NumberFormatException e2){
+                    System.out.println("***NFEx: ");
+                }
+                catch(IndexOutOfBoundsException e1){
+                    System.out.println("***IOOBEx: ");
                 }
             }
-        }
-        catch (NumberFormatException e2){
-            System.out.println("***NFEx: ");
-        }
-        catch(IndexOutOfBoundsException e1){
-            System.out.println("***IOOBEx: ");
+            System.out.println();
         }
     }
 }
